ac32f851fd3f04578f0ac66360c70a196b0501b6
Eichenherz <transferusuniversalis@gmail.com>
2021-09-26 12:26:58 +0300

    No light culling for now

--- Shaders/f_light_cull.frag.glsl
+++ Shaders/f_light_cull.frag.glsl
@@ -9,9 +9,8 @@
 #extension GL_KHR_shader_subgroup_arithmetic: require
 #extension GL_KHR_shader_subgroup_ballot: require
 
-
 layout( push_constant ) uniform block{
-	uint64_t	lightTilesAddr;
+	layout( offset = 80 ) uint64_t	lightTilesAddr;
 	uint		tileSize;
 	uint		tileRowLen;
 	uint		tileWordCount;
@@ -19,42 +18,43 @@ layout( push_constant ) uniform block{
 };
 
 
-layout( buffer_reference, buffer_reference_align = 4 ) writeonly buffer tile_ref{ 
+layout( buffer_reference, buffer_reference_align = 4 ) buffer tile_ref{ 
 	uint lightTiles[]; 
 };
 
 
 layout( location = 0 ) in flat uint lightId;
 
-const uint TILE_SIZE = 8;
-const uint TILE_WORDS_STRIDE = 1; //smth
-const uint TILE_GRID_MAX_LIGHTS_LOG2 = 1; //smth
-const uint TILE_ROW_LEN = 1; //smth
+//const uint TILE_SIZE = 8;
+//const uint TILE_WORDS_STRIDE = 1; //smth
+//const uint TILE_GRID_MAX_LIGHTS_LOG2 = 1; //smth
+//const uint TILE_ROW_LEN = 1; //smth
 
-layout( early_fragment_tests ) in;
+//layout( early_fragment_tests ) in;
 void main()
 {
-	//uvec2 tileId = uvec2( gl_FragCoord.xy ) % tileSize;
-	//uint tileIndex = tileId.y * tileRowLen + tileId.x;
-	//uint lightBit = 1u << ( lightId % 32 );
-	//uint lightWord = lightId / 32;
-	//
-	//uint wordIndex = tileIndex * tileWordCount + lightWord;
-	//
-	//uint key = wordIndex << tileMaxLightsLog2;
-	//
-	//uvec4 waveMask;
-	//for(;;)
-	//{
-	//	uint firstInvocationVal = subgroupBroadcastFirst( key );
-	//	waveMask = subgroupBallot( firstInvocationVal == key );
-	//	if( firstInvocationVal == key ) break;
-	//}
-	//
-	//uint hash = subgroupBallotInclusiveBitCount( waveMask );
-	//[[ branch ]]
-	//if( hash == 0 )
-	//{
-	//	atomicOr(  tile_ref( lightTilesAddr ).lightTiles[ wordIndex ], lightBit );
-	//}
+	atomicOr(  tile_ref( lightTilesAddr ).lightTiles[ 0 ], -1 );
+	uvec2 tileId = uvec2( gl_FragCoord.xy ) % tileSize;
+	uint tileIndex = tileId.y * tileRowLen + tileId.x;
+	uint lightBit = 1u << ( lightId % 32 );
+	uint lightWord = lightId / 32;
+	
+	uint wordIndex = tileIndex * tileWordCount + lightWord;
+	
+	uint key = wordIndex << tileMaxLightsLog2;
+	
+	uvec4 waveMask;
+	for(;;)
+	{
+		uint firstInvocationVal = subgroupBroadcastFirst( key );
+		waveMask = subgroupBallot( firstInvocationVal == key );
+		if( firstInvocationVal == key ) break;
+	}
+	
+	uint hash = subgroupBallotInclusiveBitCount( waveMask );
+	[[ branch ]]
+	if( hash == 0 )
+	{
+		atomicOr(  tile_ref( lightTilesAddr ).lightTiles[ wordIndex ], lightBit );
+	}
 }
--- Shaders/v_light_cull.vert.glsl
+++ Shaders/v_light_cull.vert.glsl
@@ -19,15 +19,16 @@ layout( buffer_reference, scalar, buffer_reference_align = 4 ) readonly buffer g
 	vec3 pos[];
 };
 
+
 layout( location = 0 ) out flat uint lightId;
 void main()
 {
-	//uint vertexIdx = uint( gl_VertexIndex & uint16_t( -1 ) );
-	//uint lightIdx = uint( gl_VertexIndex >> 16 );
-	//
-	//vec3 pos = geom_ref( geomAddr ).pos[ vertexIdx ];
-	//light_data light = light_ref( lightsAddr ).lights[ lightIdx ];
-	//
-	//gl_Position = projView * vec4( pos * light.radius + light.pos, 1.0f );
-	//lightId = lightIdx;
+	uint vertexIdx = uint( gl_VertexIndex & uint16_t( -1 ) );
+	uint lightIdx = uint( gl_VertexIndex >> 16 );
+	
+	vec3 pos = geom_ref( geomAddr ).pos[ vertexIdx ].xyz;
+	light_data light = light_ref( lightsAddr ).lights[ lightIdx ];
+
+	gl_Position = projView * vec4( pos * light.radius + light.pos, 1.0f );
+	lightId = lightIdx;
 }
