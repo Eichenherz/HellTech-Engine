35b317690b582d38c8a280d515be26807d0512f6
Eichenherz <transferusuniversalis@gmail.com>
2026-04-30 13:39:58 +0300

    Misc.

--- HtLib/ht_core_types.h
+++ HtLib/ht_core_types.h
@@ -6,22 +6,25 @@
 #include <stdint.h>
 #include <concepts>
 
-using u8 = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
+using u8    = uint8_t;
+using u16   = uint16_t;
+using u32   = uint32_t;
+using u64   = uint64_t;
 
-using i8 = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
+using i8    = int8_t;
+using i16   = int16_t;
+using i32   = int32_t;
+using i64   = int64_t;
 
 #define INVALID_IDX -1
 
 template<typename T>
-concept UINT_T = std::integral<T> && std::is_unsigned_v<T>;
+concept INTEGER_T = std::integral<T>;
 
-template<UINT_T T>
+template<typename T>
+concept UINT_T = INTEGER_T<T> && std::is_unsigned_v<T>;
+
+template<INTEGER_T T>
 inline bool IsIndexValid( T idx ) 
 { 
     constexpr T INVALID = T( INVALID_IDX );
